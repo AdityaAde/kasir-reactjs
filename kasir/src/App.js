79c5b4edf0d011c95937e9c7aaadd7e1da665372
Aditya Ade <adidtyaad@gmail.com>
2024-01-28 23:32:46 +0700

    first init

---
+++ kasir/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import NavbarComponent from './components/NavbarComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Halo</h2>
+      <NavbarComponent></NavbarComponent>
+    </div>
+  );
+}
+
+export default App;
