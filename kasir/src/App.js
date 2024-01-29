676cb1f285cafe30092d483159984940837263fc
Aditya Ade <adidtyaad@gmail.com>
2024-01-29 14:13:33 +0700

    build: show product using api

--- kasir/src/App.js
+++ kasir/src/App.js
@@ -1,27 +1,57 @@
 import './App.css';
+import React, { Component } from 'react'
 import { Col, Row, Container } from 'react-bootstrap';
-import { Hasil, ListCategories, NavbarComponent } from './components/components';
+import { Hasil, ListCategories, NavbarComponent, Menus } from './components/components';
+import { API_URL } from "./utils/constants";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <NavbarComponent></NavbarComponent>
-      <div className='mt-3'>
-        <Container fluid>
-          <Row>
-            <ListCategories></ListCategories>
-            <Col>
-              <h4>
-                <strong>Daftar Produk</strong>
-                <hr></hr>
-              </h4>
-            </Col>
-            <Hasil></Hasil>
-          </Row>
-        </Container>
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      menus: [],
+    }
+  }
+
+  componentDidMount() {
+    axios.get(`${API_URL}/products`)
+      .then(res => {
+        const menus = res.data;
+        this.setState({ menus });
+      }).catch((err) => {
+        console.log(err);
+      })
+  }
+
+  render() {
+    const { menus } = this.state;
+    return (
+      <div className="App">
+        <NavbarComponent></NavbarComponent>
+        <div className='mt-3'>
+          <Container fluid>
+            <Row>
+              <ListCategories></ListCategories>
+              <Col>
+                <h4>
+                  <strong>Daftar Produk</strong>
+                  <hr></hr>
+                  <Row>
+                    {menus && menus.map((menu) => (
+                      <Menus menu={menu} key={menu.id}></Menus>
+                    ))}
+                  </Row>
+                </h4>
+              </Col>
+              <Hasil></Hasil>
+            </Row>
+          </Container>
+        </div>
       </div>
-    </div>
-  );
+    )
+  }
 }
 
-export default App;
+
+
--- kasir/src/components/components.js
+++ kasir/src/components/components.js
@@ -1,5 +1,7 @@
 import Hasil from './Hasil'
 import ListCategories from './ListCategories'
 import NavbarComponent from './NavbarComponent'
+import Menus from './menus'
 
-export { Hasil, ListCategories, NavbarComponent }
+
+export { Hasil, ListCategories, NavbarComponent, Menus }
