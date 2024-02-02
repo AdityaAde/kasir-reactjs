f6e91b59bc55c624ad33091b37c411a4a7eb40ce
Aditya <adidtyaad@gmail.com>
2024-02-02 07:58:05 +0700

    buid: clear carts after payment

--- kasir/src/components/TotalOrders.jsx
+++ kasir/src/components/TotalOrders.jsx
@@ -1,44 +1,55 @@
 import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React, { Component } from "react";
+import axios from "axios";
+import React from "react";
 import { Col, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
+import { useNavigate } from "react-router-dom";
+import { API_URL } from "../utils/constants";
 import { numberWithCommas } from "../utils/format";
 
-export default class TotalOrders extends Component {
-  submitTotalPayments = (totalPayments) => {
+function TotalOrders(props) {
+  const navigate = useNavigate();
+
+  const submitTotalPayments = (totalPayments) => {
     const orders = {
       total_bayar: totalPayments,
-      menus: this.props.carts,
+      menus: props.carts,
     };
+
+    axios.post(API_URL + "/orders", orders).then((_) => {
+      navigate("/success");
+    });
   };
 
-  render() {
-    const totalPayments = this.props.carts.reduce(function (result, item) {
-      return result + item.total_harga;
-    }, 0);
-    return (
-      <div className="fixed-bottom">
-        <Row className="mb-2">
-          <Col md={{ span: "3", offset: "9" }} className="px-4">
-            <h4>
-              Total Harga:{" "}
-              <strong className="float-right mr-2">
-                Rp. {numberWithCommas(totalPayments)}
-              </strong>
-            </h4>
-            <div className="d-grid gap-2">
-              <Button
-                variant="primary"
-                size="lg"
-                onClick={() => this.submitTotalPayments(totalPayments)}>
-                <FontAwesomeIcon icon={faShoppingCart} />
-                <strong> Bayar</strong>
-              </Button>
-            </div>
-          </Col>
-        </Row>
-      </div>
-    );
-  }
+  const totalPayments = props.carts.reduce(function (result, item) {
+    return result + item.total_harga;
+  }, 0);
+
+  return (
+    <div className="fixed-bottom">
+      <Row className="mb-2">
+        <Col md={{ span: 3, offset: 9 }} className="px-4">
+          {" "}
+          <h4>
+            Total Harga:{" "}
+            <strong className="float-right mr-2">
+              Rp. {numberWithCommas(totalPayments)}
+            </strong>
+          </h4>
+          <div className="d-grid gap-2">
+            <Button
+              variant="primary"
+              size="lg"
+              onClick={() => submitTotalPayments(totalPayments)}>
+              <FontAwesomeIcon icon={faShoppingCart} />
+              <strong> Bayar</strong>
+            </Button>
+          </div>
+        </Col>
+      </Row>
+    </div>
+  );
 }
+
+export default TotalOrders;
--- kasir/src/pages/Success.jsx
+++ kasir/src/pages/Success.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
 import { Button, Image } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import { API_URL } from "../utils/constants";
 
 function SuccessPage() {
   const navigate = useNavigate();
+
+  useEffect(() => {
+    axios
+      .get(`${API_URL}/carts`)
+      .then((res) => {
+        const carts = res.data;
+        carts.map((cart) => {
+          return axios
+            .delete(API_URL + "/carts/" + cart.id)
+            .then((res) => {
+              console.log("res");
+            })
+            .catch((err) => {
+              console.log(`Error ${err}`);
+            });
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div className="text-center mt-4">
       <Image src="assets//images/success.png" width={500}></Image>
